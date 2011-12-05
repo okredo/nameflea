6ad2eed02789252ca51b1c5e1befa2e2a95d301d
Oren Kredo <oren.kredo@gmail.com>
2011-12-04 22:50:05 -0800

    port config

--- app.js
+++ app.js
@@ -109,8 +109,10 @@ app.put('/', function(req, res){
   //res.redirect('back');
 });
 
-app.listen(34376);
-console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
+var port = process.env.PORT || 3000;
+app.listen(port, function() {
+  console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
+});
 
 
 /*
