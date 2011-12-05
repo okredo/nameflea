3e900a6858b410fd86a90ff5cfa53bc429e30c6d
Oren Kredo <oren.kredo@gmail.com>
2011-12-04 20:53:19 -0800

    fix Error R11 (Bad bind)

--- app.js
+++ app.js
@@ -120,5 +120,5 @@ app.post('/', function(req, res){
 });
 */
 
-app.listen(3000);
+app.listen(34376);
 console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
